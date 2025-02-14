aa9c2e370b9f2971bd71f75515ba4c2cefa6733b
Edison Alexander <93294227+Sama3liz@users.noreply.github.com>
2025-02-13 16:51:28 -0800

    Update process_math.php

--- process_math.php
+++ process_math.php
@@ -4,8 +4,6 @@ $a = escapeshellarg($_POST['num1']);
 $b = escapeshellarg($_POST['num2']);
 $c = escapeshellarg($_POST['operation']);
 
-echo "$a $b $c";
-
 $command = escapeshellcmd("python3 math_operations.py $a $b $c");
 $output = shell_exec($command);
 
